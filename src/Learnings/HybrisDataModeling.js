38d553b9c00a29c7f4accc038b31b66793d45423
kshitijtechhub <151966595+kshitijtechhub@users.noreply.github.com>
2023-12-18 21:46:09 +0530

    Update HybrisDataModeling.js

--- src/Learnings/HybrisDataModeling.js
+++ src/Learnings/HybrisDataModeling.js
@@ -14,7 +14,7 @@ const HybrisDataModeling = () => {
         textContent5, textContent6 } = msg.hybrisDataModeling[0];
     return (
         <div id="hybris-items">
-        <TrackPageAnalytics pagename="HybrisItemsXml"/>
+        <TrackPageAnalytics pagename="HybrisDataModeling"/>
         <Container maxWidth="xl" sx={{ marginTop: 4 }}>          
             <Paper elevation={2} sx={{ padding: 3, borderRadius: 4 }}>
                 <Grid container spacing={2}>
